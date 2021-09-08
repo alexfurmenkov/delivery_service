889098bdfac69e251b45e06608ae1ff7e55117b6
alexfurmenkov <a210c1755>
2021-09-08 23:14:29 +0200

    [ADDED] test fixtures and environment file

---
+++ tests/test_utils/test_records_pks.py
@@ -0,0 +1,8 @@
+"""
+This file contains primary keys of the records which are created by fixtures.
+Fixtures are located in /tests/fixtures directory.
+"""
+
+
+TEST_CARRIER_PK: str = '69ddda66-25af-4ee4-b481-7945b4f3b2ba'
+TEST_ZONE_PK: str = '3e165de9-d421-41eb-abf1-1e10214280f8'
