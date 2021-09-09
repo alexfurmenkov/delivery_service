2019b56b07f5458ce0a0170412fa49fe28ad1fab
alexfurmenkov <a210c1755>
2021-09-10 00:15:42 +0200

    [ADDED] Swagger documentation

--- src/delivery_service/urls.py
+++ src/delivery_service/urls.py
@@ -7,6 +7,10 @@ more convenient than Django class based views.
 Also, the ViewSet class creates RESTful URLs under the hood
 which makes it even more convenient (see DRF docs for reference).
 """
+from django.conf.urls import url
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 import delivery_service.views as delivery_service_views
@@ -16,4 +20,13 @@ router: DefaultRouter = DefaultRouter()
 router.register(r'zones', delivery_service_views.ZonesView, basename='zones')
 router.register(r'carriers', delivery_service_views.CarriersView, basename='carriers')
 
-urlpatterns: list = router.urls
+schema_view = get_schema_view(
+   openapi.Info(title='Delivery Service API', default_version='v1'),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    url(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+urlpatterns += router.urls
